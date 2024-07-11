9c207f5c52ea326b79521924aa4ffc8b95e701f5
Andres Cano Rave <116232866+cano1988@users.noreply.github.com>
2024-07-11 18:45:03 -0500

    Add files via upload

---
+++ tallerJueves_Semana2/src/BuscaPalabras.java
@@ -0,0 +1,12 @@
+public class BuscaPalabras {
+    public static void main(String[] args){
+
+     /*   Ejercicio 5: Busca palabras.
+        Un programa que permita al usuario ingresar un texto y luego le da la
+        opción de buscar una palabra específica, mostrando como resultado
+        el rango de posiciones donde se encontró la palabra exacta. (Sin darle
+        importancia a mayúsculas o minúsculas)*/
+
+
+    }
+}
---
+++ tallerJueves_Semana2/src/BusquedaCoincidencia.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BusquedaCoincidencia {
+
+    public static void main(String[] args){
+/*
+        Ejercicio 6: Búsqueda por coincidencia en arrays
+        Crea un array de Strings (por ejemplo, nombres) y pide al usuario que
+        ingrese un nombre. Busca ese nombre en el array que imprima si se
+        encuentra la posición y si no un mensaje que no se encuentra.*/
+
+        Scanner name =new Scanner(System.in);
+
+        String [] nombres ={"andres", "jaime","santiago","federico"};
+        System.out.println("Escriba un nombre: ");
+        String nom = name.nextLine();
+
+        for(int i = 0; i < nombres.length; i++){
+            if (nombres[i].equals(nom)){
+                System.out.println("su posicion es:  " + i);
+                break;
+            }
+
+        }
+
+    }
+}
